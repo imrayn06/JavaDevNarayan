411403c65b3ce0a6ee6609d496da13f237097102
imrayn06 <66328938+imrayn06@users.noreply.github.com>
2024-03-03 16:54:58 +0000

    Conversion

---
+++ 01basics/03_Conversion.js
@@ -0,0 +1,55 @@
+//let score = null // 0
+//let score = 33 // number
+//let score = undefined //NaN not a number
+//let score = "Rayn" //NaN
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);// this will print the above comments
+// console.log(valueInNumber);
+
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// //console.log(booleanIsLoggedIn);
+
+// //1=> true; 0 => false
+// // "" => false
+// //"rayn" => true
+
+
+// let someNumber = 33
+
+// let stringNumber = String(someNumber)
+// console.log(stringNumber); // 33 output
+// console.log(typeof stringNumber); // String output
+
+//*********Operations*********/
+
+let value = 3
+let negValue = -value
+
+console.log(negValue)
+
+
+// all arithmetic operations + - / % ** *
+
+let str1 = "Hey"
+let str2 = "Rayn"
+
+let str3 = str1 + str2
+
+console.log(str3);
+// console.log(+trur); 1
+
+// console.log(1 + "2" + 3); output - 123 coversion toPrimitive
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+++gameCounter;
+console.log(gameCounter);
+
+
