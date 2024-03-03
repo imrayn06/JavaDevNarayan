fa199bf46c6fc2c95b1fb6b37815aa7c285278b2
imrayn06 <66328938+imrayn06@users.noreply.github.com>
2024-03-03 17:05:27 +0000

    comparision

---
+++ 01basics/04_comparision.js
@@ -0,0 +1,17 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+/*AVOID these type of conversions*/
+
+// console.log("2" > 1);
+// console.log(2 > "1");
+
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+
+console.log("2" === 2);
