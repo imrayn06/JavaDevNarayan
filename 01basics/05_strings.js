fb2da3e10694b7fe5453a96252131ffb3dfc48e6
imrayn06 <66328938+imrayn06@users.noreply.github.com>
2024-03-03 18:33:47 +0000

    strings

--- 01basics/05_strings.js
+++ 01basics/05_strings.js
@@ -1,4 +1,35 @@
 const name = "rayn"
 const repoCount = 20
+//Donot use
+// console.log(name + " has " + repoCount + " repositories ");
 
-console.log(name + "has" + repoCount + "repositories");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("Man-chest-er-United")
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('a'));
+
+
+
+const newString = gameName.substring(0, 8)
+console.log(newString);
+
+const anotherString = gameName.slice(-14, 4)
+console.log(anotherString);
+
+const newStringOne = "    Narayan    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://www.goo%33gle.com"
+console.log(url.replace('%33', '-'));
+
+console.log(url.includes('o'));
+console.log(url.includes('1'));
+
+console.log(gameName.split('-'));
